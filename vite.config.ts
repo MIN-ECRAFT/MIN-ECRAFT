7121ffb8d17d9dad8c85d72e6c5ef2eef3f4e4fe
MIN-ECRAFT <alwimunajad@gmail.com>
2026-01-29 09:57:49 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,13 @@
-export default defineConfig(({ mode }) => ({
-  base: "/MIN-ECRAFT/",
-  server: {
-    host: "::",
-    port: 8080,
-    hmr: {
-      overlay: false,
-    },
-  },
-  plugins: [
-    react(),
-    mode === "development" && componentTagger(),
-  ].filter(Boolean),
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+
+export default defineConfig({
+  base: "/",
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+});
